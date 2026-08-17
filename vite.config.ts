dfe4b1fe02dbb49af1be26a123ff5255ca52521d
yuichi_13 <miguelphilip246@gmail.com>
2026-08-17 11:27:05 +0800

    hosting

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  bbase: process.env.VITE_BASE_PATH || "/",
+  base: process.env.VITE_BASE_PATH || "/",
 });
